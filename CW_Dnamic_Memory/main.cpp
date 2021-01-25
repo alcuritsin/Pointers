0412630a8d02713e39e3eba4242acc9156ec6aca
alcuritsin <al.curitsin@gmail.com>
2021-01-25 21:37:14 +0500

    CW_Dnamic_Memory --add

---
+++ CW_Dnamic_Memory/main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+
+void FillRand(int arr[], const int n);
+void Print(int* arr, const int n);
+
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n; //размер массива
+	std::cout << "Введите размер массива: "; std::cin >> n;
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int* arr, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << arr[i]<<"\t";
+	}
+}
